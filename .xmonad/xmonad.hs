1514239e7fe71f14f5ab5ed74422b6c04882da64
Alexander Weidinger <alexwegoo@gmail.com>
2014-08-08 20:10:06 +0200

    Added a few features in xmonad/xmobar and removed a typo

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,23 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig (additionalKeysP)
+import System.IO (hPutStrLn)
 
-main = xmonad =<< xmobar (defaultConfig {
+main = do
+	xmproc <- spawnPipe "xmobar"
+	xmonad $ defaultConfig {
 		modMask = mod4Mask,
 		terminal = "urxvt",
-		workspaces = ["main","chat","web"]
-	} `additionalKeysP` myKeys)
+		workspaces = ["www", "chat", "office", "prog"] ++ map show [5..9],
+		logHook = dynamicLogWithPP $ xmobarPP {
+			ppOutput = hPutStrLn xmproc,
+			ppTitle = xmobarColor "#CCCCCC" "" . shorten 100,
+			ppCurrent = xmobarColor "#4A7781" ""
+		},
+		layoutHook = avoidStruts $ layoutHook defaultConfig
+	} `additionalKeysP` myKeys
 
 myKeys :: [(String, X())]
 myKeys = [
@@ -14,4 +25,5 @@ myKeys = [
 	("<XF86AudioLowerVolume>", spawn "amixer sset 'Master' 3%- -q"),
 	("<XF86AudioRaiseVolume>", spawn "amixer sset 'Master' 3%+ -q"),
 	--("<XF86AudioMicMute>", spawn ""),
-	("<XF86Launch1>", spawn "systemctl hibernate")]
+	("<XF86Launch1>", spawn "systemctl hibernate")
+]
