27acfe313ca5f9b400621cd5b33a99c9467f6cbf
Alexander Weidinger <alexwegoo@gmail.com>
2014-08-17 17:57:29 +0200

    Removed hard-coded home directory

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,8 +5,10 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig (additionalKeysP)
 import System.IO (hPutStrLn)
 import XMonad.Hooks.ManageHelpers
+import System.Directory (getHomeDirectory)
 
 main = do
+	homeDir <- getHomeDirectory
 	xmproc <- spawnPipe "xmobar"
 	xmonad $ defaultConfig {
 		modMask = mod4Mask,
@@ -19,19 +21,24 @@ main = do
 		},
 		layoutHook = avoidStruts $ layoutHook defaultConfig,
 		manageHook = manageHook defaultConfig <+> composeAll myManageHook
-	} `additionalKeysP` myKeys
+	} `additionalKeysP` (myKeys homeDir)
 
-myKeys :: [(String, X())]
-myKeys = [
+myKeys :: FilePath -> [(String, X())]
+myKeys homeDir = [
+	-- Volume control
 	("<XF86AudioMute>", spawn "amixer sset 'Master' toggle -q"),
 	("<XF86AudioLowerVolume>", spawn "amixer sset 'Master' 3%- -q"),
 	("<XF86AudioRaiseVolume>", spawn "amixer sset 'Master' 3%+ -q"),
+	-- etc.
 	("<XF86Launch1>", spawn "systemctl hibernate"),
 	("<XF86ScreenSaver>", spawn "slimlock"),
-	("<Print>", spawn "scrot '/home/alex/Pictures/Screenshots/%Y-%m-%d-%H%M%S_$wx$h.png'") ]
+	("<Print>", spawn $ "scrot '" ++ homeDir ++ "/Pictures/Screenshots/%Y-%m-%d-%H%M%S_$wx$h.png'") ]
 
 myManageHook :: [ManageHook]
 myManageHook = [
+	isFullscreen --> doFullFloat,
+	-- Applications
 	className =? "Vlc" --> doCenterFloat,
 	className =? "mpv" --> doCenterFloat,
-	isFullscreen --> doFullFloat ]
+	-- Firefox stuff
+	title =? "Firefox-Einstellungen" --> doCenterFloat ]
