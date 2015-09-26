870d641c2d50b5c7670b64ec60483b975b622276
Alexander Weidinger <alexwegoo@gmail.com>
2015-09-26 15:23:51 +0200

    Some changes to xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,26 +1,28 @@
 import XMonad
-import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Minimize
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeysP)
-import System.IO (hPutStrLn)
-import XMonad.Hooks.ManageHelpers
 import System.Directory (getHomeDirectory)
+import System.IO (hPutStrLn)
 
 main = do
 	homeDir <- getHomeDirectory
 	xmproc <- spawnPipe "xmobar"
-	xmonad $ defaultConfig {
+	xmonad $ ewmh defaultConfig {
 		modMask = mod4Mask,
 		terminal = "urxvt",
-		workspaces = ["1'www", "2'new", "3'irc", "4'mus", "5'mov'", "6'cod", "7'uni", "8'pir","9'mis"],
+		workspaces = ["1'www", "2'new", "3'irc", "4'mus", "5'mov", "6'cod", "7'uni", "8'pir","9'mis"],
 		logHook = dynamicLogWithPP $ xmobarPP {
 			ppOutput = hPutStrLn xmproc,
 			ppTitle = xmobarColor "#CCCCCC" "" . shorten 100,
 			ppCurrent = xmobarColor "#4A7781" ""
 		},
-		layoutHook = avoidStruts $ layoutHook defaultConfig,
+		layoutHook = avoidStruts $ minimize $ layoutHook defaultConfig,
 		manageHook = manageHook defaultConfig <+> composeAll myManageHook,
 		startupHook = setWMName "LG3D"
 	} `additionalKeysP` (myKeys homeDir)
@@ -33,9 +35,14 @@ myKeys homeDir = [
 	("<XF86AudioRaiseVolume>", spawn "amixer sset 'Master' 3%+ -q"),
 	-- etc.
 	("<XF86Launch1>", spawn "systemctl hibernate"),
-	("<XF86ScreenSaver>", spawn "slimlock"),
-	("<XF86Display>", spawn $ homeDir ++ ".extmo"),
-	("<Print>", spawn $ "scrot '" ++ homeDir ++ "/Pictures/Screenshots/%Y-%m-%d-%H%M%S_$wx$h.png'") ]
+	("<XF86ScreenSaver>", spawn "physlock"),
+	("<XF86Display>", spawn $ homeDir ++ "/.extmo.sh"),
+	("<Print>", spawn $ "scrot '" ++ homeDir ++ "/Pictures/Screenshots/%Y-%m-%d-%H%M%S_$wx$h.png'"),
+	-- minimize
+	("M-m", withFocused minimizeWindow),
+	("M-S-m", sendMessage RestoreNextMinimizedWin),
+	-- dmenu
+	("M-p", spawn "dmenu_run -i")]
 
 myManageHook :: [ManageHook]
 myManageHook = [
@@ -44,10 +51,9 @@ myManageHook = [
 	className =? "Vlc" --> doCenterFloat,
 	className =? "mpv" --> doCenterFloat,
 	className =? "Zathura" <&&> title =? "Drucken" --> doCenterFloat,
-	-- Firefox / Thunderbird
-	className =? "Firefox" <&&> title =? "Firefox-Einstellungen" --> doCenterFloat,
+	-- Thunderbird
 	className =? "Thunderbird" <&&> title =? "Thunderbird-Einstellungen" --> doCenterFloat,
-	-- Thunderbird calendar dialogs
 	className =? "Thunderbird" <&&> appName =? "Msgcompose" --> doCenterFloat,
 	className =? "Thunderbird" <&&> appName =? "Calendar" --> doCenterFloat,
-	className =? "Thunderbird" <&&> appName =? "Dialog" --> doCenterFloat ]
+	className =? "Thunderbird" <&&> appName =? "Dialog" --> doCenterFloat,
+	className =? "Steam" <&&> title /=? "Steam" --> doCenterFloat]
