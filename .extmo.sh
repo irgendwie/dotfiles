c92b590e38778b55a262110c531ee8860c893be1
Alexander Weidinger <alexwegoo@gmail.com>
2016-02-01 01:57:04 +0100

    Auto external monitor handling.

--- .extmo.sh
+++ .extmo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 if xrandr | grep -qs "VGA1\ connected"; then
-	xrandr --output VGA1 --left-of LVDS1 --mode 1024x768
+	xrandr --output VGA1 --right-of LVDS1 --auto
 	sh ~/.fehbg
 else
 	xrandr --auto
