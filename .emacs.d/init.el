3492bf91572e205e2e44cbeba7837813d241ee52
Alexander Weidinger <alexander.weidinger@tum.de>
2018-04-04 15:30:17 +0200

    Current and new emacs config at work.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,41 +1,52 @@
-(menu-bar-mode -1)
-(global-set-key "\C-xc" 'compile)
-
-;; https://stackoverflow.com/questions/3586089/prevent-emacs-from-mixing-tabs-and-spaces
-(add-hook 'js-mode-hook
-	  '(lambda()
-	     (setq indent-tabs-mode 't
-		   tab-width '4)))
-
-;; Markdown stuff
-(autoload 'markdown-mode "markdown-mode.el"
-  "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md\\'" . markdown-mode) auto-mode-alist))
-
-;; Haskell stuff
-(autoload 'haskell-mode "haskell-mode.el"
-  "Haskell mode for editing Haskell files" t)
-(setq auto-mode-alist
-      (cons '("\\.hs\\'" . haskell-mode) auto-mode-alist))
-
-;; Theme
-(load-theme 'tango-dark)
-
-;; ido wtf (thanks dk87)
-(ido-mode)
-
-;; alias
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; MELPA
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
 
-;; Backup
-(setq backup-directory-alist
-      `((".*" . "~/.saves")))
-(setq auto-save-file-name-transforms
-      `((".*" "~/.saves/" t)))
+;; disable annoying bars
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; load nord theme
+(load-theme 'nord t)
+
+;; disable splash screen
+(setq inhibit-startup-screen t)
+
+;; remove scratch message
+(setq initial-scratch-message "")
+
+;; org mode
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+
+(setq org-directory "~/org")
+(setq org-agenda-files (list org-directory))
+(setq org-default-notes-file (concat org-directory "/dump.org"))
+
+;; Genode cc style
+(c-add-style "genode" '("bsd"
+			(indent-tabs-mode .t)
+			(tab-width . 4)
+			(c-basic-offset . 4)))
+;; add melpa
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" default)))
+ '(package-selected-packages (quote (protobuf-mode nord-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
